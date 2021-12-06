93a7ecca3c5157943c08487ba9c4789963a7d80b
sanjar483 <bolushbekovs@gmail.com>
2021-12-06 17:24:56 +0600

    first commit

---
+++ src/MyComp.js
@@ -0,0 +1,8 @@
+function MyComp() {
+  return (
+    <>
+      <h1>JFIEOWMFKLSD</h1>
+    </>
+  );
+}
+export default MyComp;
---
+++ src/MyComponent.js
@@ -0,0 +1,9 @@
+function MyComponent() {
+  return (
+    <>
+      <h1>Iopmehd</h1>
+      <p>nfejkwfkwe</p>
+    </>
+  );
+}
+export default MyComponent;
